c44d2805418a78dce8cf5788d9b910335522b270
Liang Wu <liangwuweb@gmail.com>
2022-05-27 13:10:08 -0500

    Data fetch: using getServerSideProps to fetch data for dynamic pages

--- components/ArticleList.js
+++ components/ArticleList.js
@@ -1,10 +1,13 @@
 import articleStyles from '../styles/Article.module.css'
+import ArticleItem from './ArticleItem'
 
 const ArticleList = ({articles}) => {
   return (
     <div className={articleStyles.grid}>
       {
-        articles.map((article) => <h3>{article.title}</h3>)
+        articles.map((article) => (
+          <ArticleItem article={article}></ArticleItem>
+        ))
       } 
     </div>
     
